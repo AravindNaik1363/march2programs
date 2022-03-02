d5d914c3bbfd2bac04da2fd9d5cf17a473b7193f
AravindNaik1363 <98738331+AravindNaik1363@users.noreply.github.com>
2022-03-02 21:16:46 +0530

    using calloc to print integers and address

---
+++ calloc.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main()
+{
+    int n, i;
+    printf("Entered number of integers :");
+    scanf("%d",&n);
+     int *ptr = (int*)calloc(n , sizeof(int));
+    if (ptr == NULL)
+    {
+        printf("Memory not allocated.\n");
+        exit(0);
+    }
+    for (i = 0; i < n; ++i)
+    {
+        printf("Enter an integer:");
+        scanf("%d", ptr+i);
+    }    
+        
+    printf("integres are:");
+    for(i=0;i<n;i++)
+    {
+        printf("%d ",*(ptr+i));
+    }
+    printf("\naddress of integers are:");
+    for(i=0;i<n;i++)
+    {
+        printf("%d ",(ptr+i));
+    }
+            
+     return 0;
+}
