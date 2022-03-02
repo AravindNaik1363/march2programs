e2d5dd50c2f69680e9a744f79287da02e1de3385
AravindNaik1363 <98738331+AravindNaik1363@users.noreply.github.com>
2022-03-02 17:03:11 +0530

    Update rectangle_area.c

--- rectangle_area.c
+++ rectangle_area.c
@@ -2,9 +2,9 @@
 int find_area(int,int);//function prototype
 int main()
 {
-    int l,w,area;
+    unsigned int l,w,area;
     printf("enter length and width of rectangle:");
-    scanf("%d %d",&l,&w);
+    scanf("%u %u",&l,&w);
     area=find_area(l,w);//function call
     printf("area of rectangle=%d",area);
 
